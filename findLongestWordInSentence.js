d3cdbbeb9e2747ff5325737509ad51b99a0008e9
Aikol <imaikol@yahoo.com>
2018-12-02 16:23:15 -0800

    Add files via upload

---
+++ findLongestWordInSentence.js
@@ -0,0 +1,14 @@
+function findLongestWordInSentence(sentence) {
+  var cleaned = sentence.replace(/\W/g, " ");
+  var longest = "";
+  var splitted = cleaned.split(" ");
+  for (i = 0; i < splitted.length; i++) {
+    if (splitted[i].length > longest.length) {
+      longest = splitted[i];
+    }
+  }
+
+  return longest;
+}
+
+findLongestWordInSentence("You are awesome");
