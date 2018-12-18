22f54b6f1d67c30e8c25ce017863666af7ca531c
Aikol <imaikol@yahoo.com>
2018-12-18 01:08:20 -0800

    Add files via upload

---
+++ capitalizeFirstLetterOfWord.js
@@ -0,0 +1,7 @@
+function capitalizeFirstLetterOfWord(str) {
+  var splitted = str.split("");
+  var upperedFirst = splitted[0].toUpperCase();
+  splitted[0] = upperedFirst;
+  return splitted.join("");
+}
+capitalizeFirstLetterOfWord("hello");
