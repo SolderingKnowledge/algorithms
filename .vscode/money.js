2f511c3ec451567b042f4bd35995d498d0bd3d32
SolderingKnowledge <imaikol@yahoo.com>
2019-06-22 19:47:30 -0700

    penny

---
+++ .vscode/money.js
@@ -0,0 +1,12 @@
+const penny = (money)=>{
+    const change = [1,5,25,50, 100];
+    const result = [];
+    for(let i=0; i<change.length; i++){
+      const current = change[change.length-i-1];//i=0; change[change.length-0-1];
+      const count = Math.floor(money/current);
+      money = money%current;
+      result.unshift(count);
+    }
+    return result;
+  }
+  penny(193);
