970bd4f58e044f2f757d7059094d17b3d633b0de
SolderingKnowledge <imaikol@yahoo.com>
2019-06-23 20:42:24 -0700

    capitalize

--- capitalizeFirstLetterOfWord.js
+++ capitalizeFirstLetterOfWord.js
@@ -1,8 +1,8 @@
 //input => "hello" output => "Hello"
 
 function capitalizeFirstLetterOfWord(str) {
-	var splitted = str.split(''); //'hello'.['h','e','l','l','l','o']
+	var splitted = str.split(''); //'hello'.['h','e','l','l','o']
 	splitted[0]=splitted[0].toUpperCase();//'h'.'H'
 	return splitted.join('');
 }
-capitalizeFirstLetterOfWord('hello');
+capitalizeFirstLetterOfWord('hello');//'Hello'
