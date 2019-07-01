891f33f1aa7db818341f18ac32fdf27f79106430
Aikol Tashbolot uulu <aikoltashbolotuulu@Aikols-iMac.local>
2019-06-30 20:20:50 -0700

    same

---
+++ patterns/frequencyCounter.js
@@ -0,0 +1,14 @@
+const same =(arr1, arr2)=>{
+    if(arr1.length !== arr2.length){
+      return false;
+    }
+    for(let i=0; i<arr1.length; i++){
+      let index = arr2.indexOf(arr1[i] ** 2);
+      if(index == -1){
+        return false;
+      }
+      arr2.splice(index, 1);
+    }
+    return true;
+  }
+  same([1,2,3,2], [9,1,4,4]);
