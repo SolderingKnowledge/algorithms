0bb6f788a2ad8d48586bae82a971ea79df17d32b
SolderingKnowledge <imaikol@yahoo.com>
2019-07-01 23:15:40 -0700

    pattern: slidingWindow

---
+++ patterns/slidingWindow.js
@@ -0,0 +1,17 @@
+//find the biggest subset
+const subset = (arr, num)=>{
+    let max = 0;
+    for(let i=0; i<arr.length-num+1; i++){
+      let temp = 0;
+      for(let j=0; j<num; j++){
+        temp += arr[i+j];//temp kelip each element-ti kowoberet
+      }
+      if(temp > max){
+        max = temp;
+      }
+    }
+    return max;
+  }
+  subset([1,6,5,2,5,3,8], 2);//11
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////  
