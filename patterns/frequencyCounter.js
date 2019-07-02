7feb164da592970effb9a4033b2eade64d9326f6
SolderingKnowledge <imaikol@yahoo.com>
2019-07-01 22:29:37 -0700

    patterns

--- patterns/frequencyCounter.js
+++ patterns/frequencyCounter.js
@@ -1,3 +1,5 @@
+
+//if every element in first array has squared root sum in the second array then return true;
 const same =(arr1, arr2)=>{
     if(arr1.length !== arr2.length){
       return false;
@@ -11,4 +13,36 @@ const same =(arr1, arr2)=>{
     }
     return true;
   }
-  same([1,2,3,2], [9,1,4,4]);
+  same([1,2,3,2], [9,1,4,4]); //true;
+
+  /////////////////////////////////////////////////////////////////////////////////////////////
+
+  const exist = (arr1, arr2)=>{
+    if(arr1.length !== arr2.length){
+      return false;
+    }
+    let hash1 = {};
+    let hash2 = {};
+    for(let val of arr1){
+      hash1[val] = (hash1[val]|| 0)+1;
+    }
+    for(let val of arr2){
+      hash2[val] = (hash2[val]|| 0)+1;
+    }
+    for(let key in hash1){
+      if(!(key ** 2 in hash2)){
+        return false;
+      }
+      if(hash2[key ** 2] !== hash1[key]){
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  exist([1,5,3,2], [9,1,25,4]); //true;
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+
