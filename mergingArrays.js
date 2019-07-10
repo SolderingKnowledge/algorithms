c64ef4be4a3408c5706c17b7954c396fe01e43ac
Aikol Tashbolot uulu <aikoltashbolotuulu@Aikols-iMac.local>
2019-07-09 19:42:17 -0700

    merge arrays

--- mergingArrays.js
+++ mergingArrays.js
@@ -1,13 +1,13 @@
 //Merge 2 arrays in sorted order
 const mergeArrays = (a1, a2)=>{
     const a = a1.concat(a2);
-    let sorted = false;
-    while(!sorted){
-      sorted = true;
+    let sorted = true;
+    while(sorted){
+      sorted = false;
       for(let i=0; i<a.length; i++){
         if(a[i- 1] > a[i]){
-          sorted = false;
-          let temp = a[i-1];
+          sorted = true;
+          let temp = a[i-1];//swapping
           a[i-1]= a[i];
           a[i] = temp
         }
