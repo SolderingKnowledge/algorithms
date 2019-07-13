d824bb43c894e35f7216ad64757bf6ef20e0bf3a
Aikol Tashbolot uulu <aikoltashbolotuulu@Aikols-iMac.local>
2019-07-13 00:49:02 -0700

    matrix

--- matrix.js
+++ matrix.js
@@ -1,9 +1,40 @@
-const matrix =(num)=>{
-    let result = [];
-    for(let i=0; i<num; i++){
-        result.push([])
+const matrix = (n)=>{
+    let results = [];
+    for(let i=0; i<n; i++){
+      results.push([]);
     }
-    return result;
-}
-
-matrix(4)
+    let counter = 1;
+    let startColumn = 0;
+    let endColumn = n-1;
+    let startRow = 0;
+    let endRow = n-1;
+  
+    while(startColumn<=endColumn && startRow <= endRow){
+      //Top
+      for(let i=startColumn; i<=endColumn; i++){
+        results[startRow][i]=counter;
+        counter++;
+      }
+      startRow++;
+      //Right
+      for(let i= startRow; i<=endRow; i++){
+        results[i][endColumn]=counter;
+        counter++;
+      }
+      endColumn--;
+      //Bottom
+      for(let i=endColumn; i>=startColumn; i--){
+        results[endRow][i]= counter;
+        counter++;
+      }
+      endRow--;
+      //Middle
+      for(let i=endRow; i>=startRow; i--){
+        results[i][startColumn]=counter;
+        counter++;
+      }
+    startColumn++;
+      }
+    return results;
+  }
+  matrix(5);
