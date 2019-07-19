e1f2f0e435c8c999432af4a0516fdcb1cbea6aa1
Aikol Tashbolot uulu <aikoltashbolotuulu@Aikols-iMac.local>
2019-07-18 19:55:07 -0700

    substring

---
+++ substringLength.js
@@ -0,0 +1,10 @@
+//Find the longest substring length
+//input: 'abaabcdbc'; output: 4 =>'abcd'.length
+//input: 'ccccccccccc' output: 1 =>'c'.length
+
+
+
+const substringLength = str => {
+    f
+}
+substringLength('abaabcdbc');
