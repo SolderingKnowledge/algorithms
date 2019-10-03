497665bb205c1a5f3ac6d8f4f1ffdef253ed9f6d
Aikol Tashbolot uulu <imaikol@yahoo.com>
2019-10-02 22:57:33 -0700

    comit

--- islands.js
+++ islands.js
@@ -74,7 +74,7 @@ const islands = grid => {
     for(var i = 0; i < grid.length; i++) {
         for(var j = 0; j < grid[0].length; j++) {
             if(grid[i][j] === 1) {
-                traverseIsland(i, j, grid);
+                breadthFirst(i, j, grid);
                 count++;
             }
         }
@@ -82,27 +82,27 @@ const islands = grid => {
     
     return count;
 };
-    function traverseIsland(i, j, grid) {
-        var stack = [];
+function breadthFirst(i, j, grid) {
+    var stack = [];
+    
+    stack.push([i, j]);
+    
+    while(stack.length) {
+        var pair = stack.pop();
+        i = pair[0];
+        j = pair[1];
         
-        stack.push([i, j]);
-        
-        while(stack.length) {
-            var pair = stack.pop();
-            i = pair[0];
-            j = pair[1];
-            
-            if(i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] === 1) {
-                grid[i][j] = 2;
-                stack.push([i + 1, j]);
-                stack.push([i - 1, j]);
-                stack.push([i, j + 1]);
-                stack.push([i, j - 1]);
-            }
+        if(i >= 0 && i < grid.length && j >= 0 && j < grid[0].length && grid[i][j] === 1) {
+            grid[i][j] = 2;
+            stack.push([i + 1, j]);
+            stack.push([i - 1, j]);
+            stack.push([i, j + 1]);
+            stack.push([i, j - 1]);
         }
     }
+}
   
-  console.log(numIslands([
+  console.log(breadthFirst([
   [1,0,0,0],
   [1,1,0,0],
   [0,0,0,0],
