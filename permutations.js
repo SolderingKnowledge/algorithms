b20ec6e74b4c61687bf2fa355ea236659774187a
Aikol Tashbolot uulu <imaikol@yahoo.com>
2019-11-04 00:29:02 -0800

    permutations

--- permutations.js
+++ permutations.js
@@ -18,26 +18,26 @@ high level:
 
 function per(str, s){
     const result = [], hash = {}, arr = [str];
-    
+       
     while(arr.length){
-      let curr = arr.shift().split("");// [ 'a', '*', 'b', '*' ]
-      for(let i=0; i<curr.length; i++){
+        let curr = arr.shift().split("");// [ 'a', '*', 'b', '*' ]
+        for(let i=0; i<curr.length; i++){
         if(curr[i]=="*"){
-          for(let j=0; j<s.length; j++){
-            let c = curr.join("");
-            c=c.split("");//[ 'a', '*', 'b', '*' ]
-            c[i]=j[i];//inserting
-            if(c.includes("*")){
-              arr.push(c[i])
-            }
-            else if(!hash[c[i]]){
-              result.push(c[i]);
-              hash[c[i]]=true;
-            }
-          }
+             for(let j=0; j<s.length; j++){
+                curr[i]=s[j];//inserting
+                let c = curr.join("");
+                if(c.includes("*")){
+                    arr.push(c)
+                }
+                 else if(!hash[c]){
+                    result.push(c);
+                    hash[c]=true;
+           }
+         }
         }
       }
-    }
+     }
     return result;
-  }
-  per("a*b*", "cd");
+}
+ per("a*b*", "cd");//["adbc", "adbd", "acbc", "acbd"]
+      
