5ddd3b0704e0add72491e6529c53fdc254d05da0
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-03-28 21:42:39 -0700

    singly linked list

--- OOP/singlyLinkedList.js
+++ OOP/singlyLinkedList.js
@@ -1,12 +1,29 @@
 class Node {
-    constructor(nxt, val){
-        this.next = nxt;
+    constructor(val){
+        this.next = null;
         this.value = val;
     }
 }
 
 class SinglyLinkedList{
     constructor(){
-        
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    insert(val){
+        var newNode = new Node(val);
+        if(!this.head){
+            this.head = newNode;
+            this.tail = this.head;
+        }
+        else{
+            this.tail.next = newNode;//new SinglyLinkedList.tail.next = newNode;
+            this.tail = newNode;//new SinglyLinkedList.tail = newNode;
+        }
+        this.length++;
+        return this;
     }
 }
+
+var ssl = new SinglyLinkedList();
