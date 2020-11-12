adb05401c0ea5968387d29589b5ca9bb9eeaa83e
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-11-12 01:32:26 -0800

    string match

--- test/js.js
+++ test/js.js
@@ -1,6 +1,10 @@
 const strMatch = (candidates, pattern) => {
     let times = 0;
     for(let i=0; i<candidates.length; i++){
+        let sample = candidates.substring(i, i+pattern.length);
+        if(sample === pattern){
+            times++;
+        }
 
     }
     return times;
