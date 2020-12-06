aa80d2747d5fb3a797a06e5ccd15a60380d2a612
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-12-06 00:33:05 -0800

    Single Linked List

---
+++ singlyLinkedList/merge2Lists.js
@@ -0,0 +1,21 @@
+const a = {val:1, next:{val:2, next:{val:3, next:null}}};
+const b = {val:3, next:{val:4, next:{val:5, next:{val:50, next:{val:100, next:null}}}}};
+
+function mergeTwoLists(a, b){
+    const result = {val:null, next:null}
+    const curr = result;
+
+    while (a && b) {
+        if(a.val < b.val){
+            curr.next = a;
+            a = a.next;
+        } else {
+            curr.next = b;
+            b = b.next;
+        }
+        curr = curr.next;
+    }
+    curr.next = a || b;
+    return result.next;
+}
+console.log(sortll(a, b));
