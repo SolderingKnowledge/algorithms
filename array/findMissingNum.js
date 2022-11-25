e179ba7a161f7c9a92efd2c8bfd98e7327585778
Aikol <atashbol@starbucks.com>
2022-11-24 23:09:50 -0800

    Reformat

--- array/findMissingNum.js
+++ array/findMissingNum.js
@@ -14,13 +14,14 @@
 // findMissingNum([ 1, 2, 3, 5, 6, 7, 8, 9, 10 ]);
 
 function findMissingNum(numbers) {
-	var missing = [];
+	var missing = []
 	for (var i = 0; i <= numbers.length; i++) {
-		if (numbers[i+1]- numbers[i] > 1){//next - current > 1 
-			missing.push(numbers[i+1]-numbers[0]);//if it starts with numbers[0]=1 then start from 0 index 
-			//missing.push(numbers[i+1]-numbers[1]);//if it starts with numbers[0]=0 then start from 1 index 
+		if (numbers[i + 1] - numbers[i] > 1) {
+			//next - current > 1
+			missing.push(numbers[i + 1] - numbers[0]) //if it starts with numbers[0]=1 then start from 0 index
+			//missing.push(numbers[i+1]-numbers[1]);//if it starts with numbers[0]=0 then start from 1 index
 		}
 	}
-	return missing;
+	return missing
 }
-findMissingNum([1, 2, 3, 5, 6, 7, 8, 9, 10, 12 ]);
+findMissingNum([1, 2, 3, 5, 6, 7, 8, 9, 10, 12])
