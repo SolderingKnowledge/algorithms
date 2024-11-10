b848670284b7be7273fbc18064902137e0f0edb8
atashbol@starbucks.com <atashbol@starbucks.com>
2024-11-10 01:28:29 -0800

    Update numOfIslands

--- backtracking/islands.js
+++ backtracking/islands.js
@@ -100,14 +100,53 @@ function islands(grid) {
 	return count
 }
 
-console.log(
-	islands([
-		['1', '1', '1', '1', '0'],
-		['1', '1', '0', '1', '0'],
-		['1', '1', '0', '0', '0'],
-		['0', '0', '0', '0', '0'],
-	])
-)
+const grid = [
+	['1', '1', '1', '1', '0'],
+	['1', '1', '0', '1', '0'],
+	['1', '1', '0', '0', '0'],
+	['0', '0', '0', '0', '0'],
+]
+
+console.log(islands(grid))
+
+const grid2 = [
+	['1', '1', '1', '1', '0'],
+	['1', '1', '0', '1', '0'],
+	['1', '1', '0', '0', '1'],
+	['0', '0', '0', '0', '1'],
+]
+
+const numIslands = (grid) => {
+	if (!grid || grid.length === 0) return 0
+
+	let count = 0
+
+	const dfs = (grid, i, j) => {
+		// Base case: if out of bounds or at water, return
+		if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') return
+
+		// Mark the cell as visited by setting it to '0'
+		grid[i][j] = '0'
+
+		// Visit all 4 adjacent cells
+		dfs(grid, i + 1, j) // down
+		dfs(grid, i - 1, j) // up
+		dfs(grid, i, j + 1) // right
+		dfs(grid, i, j - 1) // left
+	}
+
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			if (grid[i][j] === '1') {
+				count++
+				dfs(grid, i, j)
+			}
+		}
+	}
+
+	return count
+}
+console.log(numIslands(grid2))
 
 /*
 high level:
